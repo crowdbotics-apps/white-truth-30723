1e475392a8f99d82293a016e5e06b8e0723bf654
Crowdbotics <team@crowdbotics.com>
2021-09-21 15:36:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_truth_30723Identifier",
+  url: "https://white_truth_30723Identifier.botics.co",
+  api: "https://white_truth_30723Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
